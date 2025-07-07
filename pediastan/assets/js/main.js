ec79bfe641ed159a32cafbc4b41beacfee715b28
Khansa Irfan <190067244+KhansaIrfan@users.noreply.github.com>
2025-07-07 18:55:50 +0500

    Add javascript functionality to change background images on hero section

--- pediastan/assets/js/main.js
+++ pediastan/assets/js/main.js
@@ -18,24 +18,240 @@ rightSideBar.addEventListener('click', function () {
 
 
 /*code for image carousel*/
-let img = document.querySelector(".herosection-column-1");
-let accessKey = "hsUnn4EcKnH88irrTEpnnz6cHrjJcDLEXxJW-99mHic";
-const query = "Pakistan";
+let news = document.querySelector(".hero-section");
+let apiKey = "200edffdfb4c4b91a6d36bd7d427ea3d";
+let imgAccessKey = "hsUnn4EcKnH88irrTEpnnz6cHrjJcDLEXxJW-99mHic";
+let newsTitle = document.querySelectorAll("#hero-section-link");
+let img1 = document.querySelector(".herosection-column-1");
+let img2 = document.querySelector(".herosection-col-2-row-1-col-1");
+let query2 = "Lahore";
+let query = "Karachi";
 
-let slider = [];
-let id = 0;
+async function fetchNews() {
+  try {
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
+    const data = await response.json();
 
-fetch(`https://api.unsplash.com/search/photos?query=${query}&per_page=5&client_id=${accessKey}`)
-.then(res => res.json())
-.then(data => {
-  slider = data.results.map(img => img.urls.full);
-  img.style.backgroundImage = `url(${slider[0]})`;
-  setInterval(() => {
-    id = (id + 1) % slider.length;
-    img.style.backgroundImage = `url(${slider[id]})`;
-  }, 9000);
-})
-.catch(err => console.error("Error fetching images:", err));
+    if (data.articles && data.articles.length > 0) {
+      let newsHTML = "";
+
+      data.articles.slice(0, 1).forEach(article => {
+        newsHTML += `
+          <div class="col-lg-6 col-6 col-md-12 col-12">
+            <a href="${article.url}" title="${article.title}" target="_blank">
+              <div class="herosection-column-1">
+                <div class="content">
+                  <div class="above-category">
+                    <a href="" class="text-capitalize" id="hero-section-link" target="_blank">
+                      <span>
+                        cricket
+                      </span>
+                    </a>
+                  </div>
+                  <div class="hero-section-title">
+                    <h3>
+                      <a href="${article.url}" title="${article.title}" target="_blank" id="hero-section-link">
+                        ${article.title}
+                      </a>
+                    </h3>
+                  </div>
+                  <div class="date">
+                    <div class="date-bg">
+                      June 26, 2025
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-6 col-md-12 col-12 herosection-col-2">
+            <div class="d-flex flex-wrap gx-0 row-gap-1">
+              <div class="d-flex column-gap-1">
+                <div class="herosection-col-2-row-1-col-1">
+                  <div class="col-2-content">
+                    <div class="hero-section-right-title">
+                      <h3>
+                        <a href="${article.url}" title="${article.title}" id="hero-section-link">
+                        ${article.title}
+                        </a>
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+                <div class="herosection-col-2-row-1-col-1">
+                  <div class="col-2-content">
+                    <div class="hero-section-right-title">
+                      <h3>
+                        <a href="${article.url}" title="${article.title}" id="hero-section-link">
+                        ${article.title}
+                        </a>
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="d-flex column-gap-1">
+                <div class="herosection-col-2-row-1-col-1">
+                  <div class="col-2-content">
+                    <div class="hero-section-right-title">
+                      <h3>
+                        <a href="${article.url}" title="${article.title}"  id="hero-section-link">
+                        ${article.title}
+                        </a>
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+    
+                <div class="herosection-col-2-row-1-col-1">
+                  <div class="col-2-content">
+                    <div class="hero-section-right-title">
+                      <h3>
+                        <a href="${article.url}" title="${article.title}" id="hero-section-link">
+                        ${article.title}
+                        </a>
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+    
+            </div>
+          </div>
+        `;
+      });
+
+
+      news.innerHTML = newsHTML;
+      fetchImages();
+    } else {
+      news.innerHTML = "<p>No news found.</p>";
+    }
+
+
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    news.innerHTML = "<p>Failed to load news.</p>";
+  }
+}
+
+
+
+
+
+function fetchImages() {
+  let img1 = document.querySelector(".herosection-column-1");
+  let img2 = document.querySelectorAll(".herosection-col-2-row-1-col-1");
+  let img3 = document.querySelector(".herosection-col-2-row-1-col-2");
+  let img4 = document.querySelector(".herosection-col-2-row-2-col-1");
+  let img5 = document.querySelector(".herosection-col-2-row-2-col-2");
+  let query1 = "Karachi";
+  let query2 = "Islamabad"
+  let query3 = "Lahore"
+  let query4 = "Faisalabad"
+  let query5 = "Rahimyarkhan"
+
+  fetch(`https://api.unsplash.com/search/photos?query=${query1}&per_page=5&client_id=${imgAccessKey}`)
+    .then(res => res.json())
+    .then(data => {
+      let lahoreSlider = data.results.map(img => img.urls.full);
+      if (img1) {
+        img1.style.backgroundImage = `url(${lahoreSlider[0]})`;
+        console.log("1st Section exist");
+        let lahoreIndex = 0;
+        setInterval(() => {
+          lahoreIndex = (lahoreIndex + 1) % lahoreSlider.length;
+          img1.style.backgroundImage = `url(${lahoreSlider[lahoreIndex]})`;
+        }, 7000);
+      }
+    })
+    .catch(err => console.error("Error fetching Lahore images:", err));
+
+  fetch(`https://api.unsplash.com/search/photos?query=${query2}&per_page=5&client_id=${imgAccessKey}`)
+
+    .then(res => res.json())
+    .then(data => {
+      let karachiSlider = data.results.map(img => img.urls.full);
+      for (let key in img2) {
+        if (img2[key]) {
+          let karachiIndex = 0;
+          img2[key].style.backgroundImage = `url(${karachiSlider[karachiIndex]})`;
+          img2[key].style.backgroundSize = "cover";
+          img2[key].style.display = "flex";
+          img2[key].style.alignItems = "flex-end";
+          img2[key].style.backgroundPosition = "bottom";
+          img2[key].style.height = "228px";
+          img2[key].style.overFlow = "hidden"
+          console.log(" 2nd Section exist");
+
+          setInterval(() => {
+            karachiIndex = (karachiIndex + 1) % karachiSlider.length;
+            img2[key].style.backgroundImage = `url(${karachiSlider[karachiIndex]})`;
+          }, 7000);
+        }
+
+      }
+    })
+    .catch(err => console.error("Error", err));
+
+
+  fetch(`https://api.unsplash.com/search/photos?query=${query3}&per_page=5&client_id=${imgAccessKey}`)
+    .then(res => res.json())
+    .then(data => {
+      let islamabadSlider = data.results.map(img => img.urls.full);
+      if (img3) {
+        let islamabadIndex = 0;
+        img3.style.backgroundImage = `url(${islamabadSlider[islamabadIndex]})`;
+        console.log(" 3rd Section exist");
+
+        setInterval(() => {
+          islamabadIndex = (islamabadIndex + 1) % islamabadSlider.length;
+          img3.style.backgroundImage = `url(${islamabadSlider[islamabadIndex]})`;
+        }, 7000);
+      }
+    })
+    .catch(err => console.error("Error", err));
+
+
+
+  fetch(`https://api.unsplash.com/search/photos?query=${query4}&per_page=5&client_id=${imgAccessKey}`)
+    .then(res => res.json())
+    .then(data => {
+      let faisalAbadSlider = data.results.map(img => img.urls.full);
+      if (img4) {
+        let faisalAbadIndex = 0;
+        img4.style.backgroundImage = `url(${faisalAbadSlider[faisalAbadIndex]})`;
+        console.log(" 4th Section exist");
+
+        setInterval(() => {
+          faisalAbadIndex = (faisalAbadIndex + 1) % faisalAbadSlider.length;
+          img4.style.backgroundImage = `url(${faisalAbadSlider[faisalAbadIndex]})`;
+        }, 7000);
+      }
+    })
+    .catch(err => console.error("Error", err));
+
+  fetch(`https://api.unsplash.com/search/photos?query=${query5}&per_page=5&client_id=${imgAccessKey}`)
+    .then(res => res.json())
+    .then(data => {
+      let islamabadSlider = data.results.map(img => img.urls.full);
+      if (img5) {
+        let islamabadIndex = 0;
+        img5.style.backgroundImage = `url(${islamabadSlider[islamabadIndex]})`;
+        console.log(" 5th Section exist");
+
+        setInterval(() => {
+          islamabadIndex = (islamabadIndex + 1) % islamabadSlider.length;
+          img5.style.backgroundImage = `url(${islamabadSlider[islamabadIndex]})`;
+        }, 7000);
+      }
+    })
+    .catch(err => console.error("Error", err));
+
+}
+
+fetchNews();
+
 
 
 
@@ -99,7 +315,6 @@ dateElement.innerHTML = `${formattedDate} &nbsp; - &nbsp;`;
 
 
 
-fetch('https://fakestoreapi.com/products/1')
-            .then(res=>res.json())
-            .then(json=>console.log(json))
-            
+// fetch('https://fakestoreapi.com/products/1')
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
